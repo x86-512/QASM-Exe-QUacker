48436f75e31abd5173ba09780a6e73dc46b5b4d9
Under The Bus <70492320+x86-512@users.noreply.github.com>
2024-10-17 21:39:36 +0300

    Add files via upload

--- exe_hax.py
+++ exe_hax.py
@@ -3,12 +3,21 @@ from qiskit import circuit
 from qiskit import qasm3
 from random import randint
 from qiskit.primitives import Sampler
+#from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime import QiskitRuntimeService
 from qiskit.circuit.library import MCMT, GroverOperator, MCXGate
 
 from qiskit_aer import AerSimulator
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 
 from qiskit.visualization import plot_bloch_multivector
+
+
+from sys import argv
+
+def open_file(exe_name):
+    with open(exe_name) as exe:
+        return exe.read()[0:2]
 
 def calculate_iterations(n_qubits):
     return int((pi/4)/(asin(1/sqrt(1<<n_qubits)))-0.5)
@@ -18,7 +27,7 @@ def calculate_iterations(n_qubits):
 def create_oracle(ciphertext):
     ciphertext_bytes = f"{ciphertext:08b}"[::-1]
     target = 77
-    target_bytes = f"{target:08b}"[::-1]
+    #target_bytes = f"{target:08b}"[::-1]
 
     #x = 45
     #x_bytes = f"{x:08b}"[::-1]
@@ -77,43 +86,70 @@ def create_oracle(ciphertext):
 #Apply <-> gate to register in superposition and the target until you get 1*8
 #If and only if
 #Apply !^
+def unencrypt_exe(key:int):
+    crypted_str = b""
+    with open(argv[1], 'rb') as exe:
+        for i in exe.read()[:-1]: #Skip the added \n
+            #print(int(i))
+            #print((int(i)^key))
+            #print(bytes(int(i)^key))
+            crypted_str+=(int(i)^key).to_bytes(1, 'little')
+    #print(crypted_str)
+    with open(argv[1], 'wb') as exe:
+        exe.write(crypted_str)
+    print("Successfully decrypted "+argv[1])
 
 def main():
-    n_iterations = calculate_iterations(8)
-    ciphertext = 96
+    ciphertext = ord(open_file(argv[1])[0])
+    #n_iterations = calculate_iterations(8)
+    #print(ciphertext)
+    #print(ciphertext^77)
+    #ciphertext = 96
     ciphertext_bytes = f"{ciphertext:08b}"
     target = 77
     target_bytes = f"{target:08b}"
-    qreg = circuit.QuantumRegister(9, 'q')
-    creg = circuit.ClassicalRegister(8, 'c')
-    grover_circ = circuit.QuantumCircuit(qreg, creg)
 
     #Uniform superposition
-    grover_circ.h(qreg[i] for i in range(8))
+    cracked_key = 0
+    while True:
 
-    my_oracle = create_oracle(ciphertext)
+        qreg = circuit.QuantumRegister(9, 'q')
+        creg = circuit.ClassicalRegister(8, 'c')
+        grover_circ = circuit.QuantumCircuit(qreg, creg)
 
-    grover_operator = GroverOperator(my_oracle, insert_barriers=True)
+        grover_circ.h(qreg[i] for i in range(8))
 
-    for i in range(17): #17
-        #grover_circ.compose(my_oracle, inplace=True)
-        grover_circ.compose(grover_operator, inplace=True)
-        #grover_circ.append(grover_operator, range(9))
+        my_oracle = create_oracle(ciphertext)
+
+        grover_operator = GroverOperator(my_oracle, insert_barriers=True)
+
+        for i in range(17): #17
+            #grover_circ.compose(my_oracle, inplace=True)
+            grover_circ.compose(grover_operator, inplace=True)
+            #grover_circ.append(grover_operator, range(9))
+            
+            #grover_circ.h(qreg[i] for i in range(8))
+            #grover_circ.x(qreg[i] for i in range(8))
+            #grover_circ.h(7)
+            #grover_circ.cz(qreg[:7],7)
+            #grover_circ.h(7)
+            #grover_circ.x(qreg[i] for i in range(8))
+            #grover_circ.h(qreg[i] for i in range(8))
         
-        #grover_circ.h(qreg[i] for i in range(8))
-        #grover_circ.x(qreg[i] for i in range(8))
-        #grover_circ.h(7)
-        #grover_circ.cz(qreg[:7],7)
-        #grover_circ.h(7)
-        #grover_circ.x(qreg[i] for i in range(8))
-        #grover_circ.h(qreg[i] for i in range(8))
-    
-    grover_circ.measure(qreg[:8], creg)
+        grover_circ.measure(qreg[:8], creg)
 
 
-    sampler = Sampler()
-    job = sampler.run(grover_circ, shots=1) #Weak amplification
-    print(job.result())
+        sampler = Sampler()
+        job = sampler.run(grover_circ, shots=1) #Weak amplification
+        #print(job.result())
+        #print(type(job.result()))
+        result_str = str(job.result())
+        cracked_key = int(result_str[result_str.find('{')+1:result_str.find(':')]) #I will not run multiple shots
+        if ciphertext^cracked_key == target:
+            break
+
+    print(f"Cracked Key: {cracked_key}, Hex: {hex(cracked_key)}")
+    unencrypt_exe(cracked_key)
     
     #backend = AerSimulator(method='statevector')
         
